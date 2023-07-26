6ff91ef8dc9082bc1e9350299bd345adac69d5ad
karandeep-3154 <ksingh310504@gmail.com>
2023-07-26 11:04:45 +0530

    Profile Side Design Completed

--- client/src/Pages/ProfileCard/FollowersCard/FollowersCard.js
+++ client/src/Pages/ProfileCard/FollowersCard/FollowersCard.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import './FollowersCard.css'
+import React from "react";
+import "./FollowersCard.css";
+import { Followers } from "../../../data/FollowersData";
 const FollowersCard = () => {
   return (
-    <div className='FollowersCard'>
+    <div className="FollowersCard">
       <h3>Who is Following you</h3>
-    </div>
-  )
-}
 
-export default FollowersCard
+      {Followers.map((follower, id) => {
+        return (
+          <div className="follower">
+            <div>
+              <img src={follower.image} alt="" className='followerImage' />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className="button fc-button">Follow</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FollowersCard;
---
+++ client/src/data/FollowersData.js
@@ -0,0 +1,11 @@
+// import {img1} from '../img/cover.png'
+// import {img2} from '../img/img2.png'
+// import {img3} from '../img/img3.png'
+// import {img4} from '../img/img4.jpg'
+
+export const Followers = [
+  { name: "Andrew Thomas", username: "AndrewThomas" },
+  { name: "Hulk Buster", username: "HulkBuster" },
+  { name: "Thor", username: "ThunderMaster" },
+  { name: "Natasha", username: "Natasha" },
+];
